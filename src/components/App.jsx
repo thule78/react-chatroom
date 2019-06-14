3731f3d2c73c2ed3ddd0cdfe27c5cdc8a0ccd70d
thule <lunthu@gmail.com>
2019-06-14 10:48:58 +0700

    install chatkit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { ChatManager, TokenProvider } from '@pusher/chatkit-client'
 
+import { tokenUrl, instanceLocator } from '../config'
 import ChatForm from './ChatForm';
 import ChatMessage from './ChatMessage';
 import ChatChannel from './ChatChannel';
 import NewChannel from './NewChannel';
 
-const App = () =>{
-  return(
+class App extends Component {
+  componentDidMount(){
+    const chatManager = new ChatManager({
+      instanceLocator: 'v1:us1:98503d45-efa0-4d8e-a2ae-e1d9f2d5c83c',
+      userId: 'thule',
+      tokenProvider: new TokenProvider({ url: tokenUrl
+
+
+       })
+    })
+    chatManager.connect()
+    .then(currentUser => {
+      currentUser.subscribeToRoom({
+        roomId: "chatapp",
+        messageLimit: 20,
+        hooks: {
+          onNewMessage: message => {
+            console.log('message.text: ', message.text);
+          }
+        }
+      })
+    })
+  }
+  render(){
+    return(
     <div className="container">
       <h1>Hello Chat</h1>
       <ChatForm />
@@ -15,6 +40,8 @@ const App = () =>{
       <NewChannel />
     </div>
     )
+  }
+
 }
 
 export default App
---
+++ src/config.jsx
@@ -0,0 +1,5 @@
+const tokenUrl =
+"https://us1.pusherplatform.io/services/chatkit_token_provider/v1/98503d45-efa0-4d8e-a2ae-e1d9f2d5c83c/token";
+const instanceLocator = "v1:us1:98503d45-efa0-4d8e-a2ae-e1d9f2d5c83c"
+
+export { tokenUrl, instanceLocator }
